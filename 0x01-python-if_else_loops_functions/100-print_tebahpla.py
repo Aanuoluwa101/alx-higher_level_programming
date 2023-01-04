ac75f08cb91c6c3ca85a27f52ac5155478ed5fc4
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-01-04 03:45:03 -0800

    update 12 and 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-for _ in range(ord('z'), ord('a') - 1, -1):
-    if _ % 2 == 0:
-        print(f"{chr(_)}", end="")
+def letter(n):
+    if n % 2 == 0:
+        return n
     else:
-        print(f"{chr(_ - 32)}", end="")
+        return (n - 32)
+
+
+for n in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(letter(n))), end="")
