b3efd4c00ba269bfc0aa54c9d823b039ae28ed1a
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-01-04 02:29:01 -0800

    update 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) >= 97 and ord(c) <= 122:
-        return True
-    else:
-        return False
-
-
 def uppercase(str):
     str1 = ""
     for i in range(len(str)):
-        if islower(str[i]):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             str1 += chr(ord(str[i]) - 32)
         else:
             str1 += str[i]
