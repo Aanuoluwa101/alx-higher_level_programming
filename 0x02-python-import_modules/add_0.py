b15ac01766ae0e66361d73ee4d1b45c1ff2dc8c1
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-01-05 12:08:44 -0800

    first commit

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Args:
+       a: first number
+       b: second number
+
+    Returns:
+        The sum of a and b
+    """
+    return (a + b)
