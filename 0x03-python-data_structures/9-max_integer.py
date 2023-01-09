8611ab7518ca71d9423db35a2e93bd785ee2ace2
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-01-09 08:42:19 -0800

    correcting max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,6 +2,9 @@
 def max_integer(my_list=[]):
     """Finds the biggest number in a list"""
 
+    if len(my_list) == 0:
+        return None
+
     max_int = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] > max_int:
