c4947cc1b64d192dbfbef01cea8246eca07506a3
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-01-23 20:58:38 -0800

    3 to 101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
+        return None
