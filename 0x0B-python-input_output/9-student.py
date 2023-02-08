8c81ba51696364e86b944692bdc5e730285336b3
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-02-07 21:03:47 -0800

    adding 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,4 +9,10 @@ class Student:
             raise TypeError("first and last name must be strings")
         if type(age) != int:
             raise TypeError("age must be an integer")
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
+        def to_json(self):
+            """retrieves a dictionary representation of the student"""
+            return self.__dict__
