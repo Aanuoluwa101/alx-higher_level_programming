eb16c6490ec5b84e54be38d26099ea8777835e37
Aanuoluwa101 <aanuayodeji101@gmail.com>
2023-02-08 18:53:24 -0800

    corrections

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,14 +2,15 @@
 """Defines a function that checks the class of an object"""
 
 
-def is_same_class(obj, a_class):
-    """Tells if an object is exactly an instance of a specified class
+if __name__ == "__main__":
+    def is_same_class(obj, a_class):
+        """Tells if an object is exactly an instance of a specified class
 
-    Args:
-        obj (any): the object
-        a_class (type): the class against which the object is checked
-    Returns:
-        True if the object is exactly an instance of the specified class
-        False if otherwise
-    """
-    return (type(obj) == a_class)
+        Args:
+            obj (any): the object
+            a_class (type): the class against which the object is checked
+        Returns:
+            True if the object is exactly an instance of the specified class
+            False if otherwise
+        """
+        return (type(obj) == a_class)
--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,16 @@
 #!usr/bin/python3
+"""Defines that confirms the class of an object"""
 
-def is_kind_of_class(obj, a_class):
-    """Tells if an object is exactly an instance of a class or its subclass
 
-    Args:
-        obj (any): the object
-        a_class (type): the class against which the object is checked
-    Returns:
-        True if the object is an instance of the specified class
-        or one of its subclasses or False if otherwise
-    """
-    return isinstance(obj, a_class)
+if __name__ == "__main__":
+    def is_kind_of_class(obj, a_class):
+        """Tells if an object is exactly an instance of a class or its subclass
+
+        Args:
+            obj (any): the object
+            a_class (type): the class against which the object is checked
+        Returns:
+            True if the object is an instance of the specified class
+            or one of its subclasses or False if otherwise
+        """
+        return isinstance(obj, a_class)
